de28bff7bebec2c6e4d2e3b9cac0c6c60c41b75b
Hardik Patel <hpatel@wsgc.com>
2019-06-12 16:39:35 -0700

    83.1% Acceptance

---
+++ jewels-and-stones.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function (J, S) {
+    let count = 0;
+    S.split("").forEach(chara => {
+        if (J.indexOf(chara) > -1) {
+            count = count + 1;
+        }
+    });
+    return count;
+};
