fcf6785fb5a54e7fd639f1e28e4403fc4c06cff1
Hardik Patel <hpatel@wsgc.com>
2019-08-12 07:17:36 -0700

    numTilePossibilities

---
+++ letter-tile-possibilities.js
@@ -0,0 +1,24 @@
+const numTilePossibilities = (myString, currentString = "", permutations = []) => {
+    if (permutations) {
+        permutations.push(currentString);  // Add currentString to permutations
+
+    }
+    if (!myString) {
+        return;
+    }
+
+    const length = myString.length;
+    for (let i = 0; i < length; i++) {
+        currentString += myString[i]; // Add current letter to currentString
+
+        numTilePossibilities(myString.slice(0, i) + myString.slice(i + 1), currentString, permutations); // Remove up to current letter from currentString since it is already added to currentString and recursively call the function
+
+        const permutationsLength = currentString.length;
+        currentString = currentString.slice(0, permutationsLength - 1); // Remove last letter from the currentString
+    }
+
+    const unique = [...new Set(permutations)];
+    return unique.length - 1;
+};
+
+console.log(numTilePossibilities(`AAB`));
---
+++ string-permutation.js
@@ -0,0 +1,43 @@
+// string permutation
+const getPermutation = (myString, currentString = "", permutations = []) => {
+    if (!myString) {
+        permutations.push(currentString);  // Add currentString to permutations
+        return;
+    }
+
+    const length = myString.length;
+    for (let i = 0; i < length; i++) {
+        currentString += myString[i]; // Add current letter to currentString
+
+        getPermutation(myString.slice(0, i) + myString.slice(i + 1), currentString, permutations); // Remove up to current letter from currentString since it is already added to currentString and recursively call the function
+
+        const permutationsLength = currentString.length;
+        currentString = currentString.slice(0, permutationsLength - 1); // Remove last letter from the currentString
+    }
+
+    return permutations;
+};
+console.log(getPermutation("ABC"));
+
+
+//without duplicates
+const getPermutationWithoutDuplicates = (myString, currentString = "", permutations = []) => {
+    if (!myString) {
+        permutations.push(currentString);  // Add currentString to permutations
+        return;
+    }
+
+    const length = myString.length;
+    for (let i = 0; i < length; i++) {
+        currentString += myString[i]; // Add current letter to currentString
+
+        getPermutationWithoutDuplicates(myString.slice(0, i) + myString.slice(i + 1), currentString, permutations); // Remove up to current letter from currentString since it is already added to currentString and recursively call the function
+
+        const permutationsLength = currentString.length;
+        currentString = currentString.slice(0, permutationsLength - 1); // Remove last letter from the currentString
+    }
+
+    return permutations;
+};
+
+console.log(getPermutationWithoutDuplicates("ABCA"));
