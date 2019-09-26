547a488f83d7ac5d8c3bf4f37553f06d02453e9d
Hardik Patel <hpatel@wsgc.com>
2019-09-26 06:20:03 -0700

    Create self-dividing-numbers.js

---
+++ self-dividing-numbers.js
@@ -0,0 +1,23 @@
+const selfDividingNumbers = (left, right) => {
+    const output = [];
+    for (let i = left; i <= right; i++) {
+        let digitsString = [...i + ''];
+        if (!(digitsString.indexOf('0') > -1)) {
+            const digits = digitsString.map(digit => + digit); // + returns the number representation of the object,
+            const numberOfDigits = digits.length;
+            for (let j = 0; j < numberOfDigits; j++) {
+                const currentDigit = digits[j];
+                if (i % currentDigit === 0) {
+                    if (j === numberOfDigits - 1) {
+                        output.push(i);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    } return output;
+};
+
+const result = selfDividingNumbers(232, 708);
+console.log(result);
