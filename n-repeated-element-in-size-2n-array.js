3a169d4afafc378ba703dc2c5331b53342ef4161
Hardik Patel <hpatel@wsgc.com>
2019-12-09 07:12:56 -0800

    Create n-repeated-element-in-size-2n-array.js

---
+++ n-repeated-element-in-size-2n-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var repeatedNTimes = function (A) {
+    let map = new Map();
+
+    for (let i = 0, length = A.length; i < length; i++) {
+        currentElement = A[i];
+        if (map.has(currentElement)) {
+            return currentElement;
+        } else {
+            map.set(currentElement, "this number exists");
+        }
+    }
+};
+
+console.log(repeatedNTimes([1, 2, 2, 3]));
